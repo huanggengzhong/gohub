10533e5acfa40387b15b57184e94474f0f569b93
罗庚中 <huanggengzhong@daishujiankang.com>
2023-12-14 16:30:22 +0800

    feat:解决缓存问题

--- pkg/gpt/gpt.go
+++ pkg/gpt/gpt.go
@@ -19,34 +19,30 @@ var (
 // 发送请求
 func SendPostRequest(url, data string, headers map[string]string) ([]byte, error) {
 
-	// 使用sync.Once确保初始化操作只执行一次
-	once.Do(func() {
-		// 构造消息体
-		requestData := map[string]interface{}{
-			"model": config.Get("gpt.gpt_type"),
-			"messages": []map[string]string{
-				{"role": "user", "content": data},
-			},
-		}
-		// 将数据转换为JSON字符串
-		jsonData, e := json.Marshal(requestData)
-		logger.LogIfR(e)
-		// 创建一个POST请求
-		request, e := http.NewRequest("POST", url, strings.NewReader(string(jsonData)))
-		logger.LogIfR(err)
-		// 设置请求头
-		for key, value := range headers {
-			request.Header.Set(key, value)
-		}
-		// 发送请求
-		response, e := http.DefaultClient.Do(request)
-		logger.LogIfR(e)
-		defer response.Body.Close()
-		// 读取响应体
-		body, e = io.ReadAll(response.Body)
-		logger.LogIfR(e)
-
-	})
+	// 构造消息体
+	requestData := map[string]interface{}{
+		"model": config.Get("gpt.gpt_type"),
+		"messages": []map[string]string{
+			{"role": "user", "content": data},
+		},
+	}
+	// 将数据转换为JSON字符串
+	jsonData, e := json.Marshal(requestData)
+	logger.LogIfR(e)
+	// 创建一个POST请求
+	request, e := http.NewRequest("POST", url, strings.NewReader(string(jsonData)))
+	logger.LogIfR(err)
+	// 设置请求头
+	for key, value := range headers {
+		request.Header.Set(key, value)
+	}
+	// 发送请求
+	response, e := http.DefaultClient.Do(request)
+	logger.LogIfR(e)
+	defer response.Body.Close()
+	// 读取响应体
+	body, e = io.ReadAll(response.Body)
+	logger.LogIfR(e)
 
 	return body, err
 }
@@ -54,37 +50,33 @@ func SendPostRequest(url, data string, headers map[string]string) ([]byte, error
 // 发送请求
 func SendGetRequest(url, data string, headers map[string]string) ([]byte, error) {
 
-	// 使用sync.Once确保初始化操作只执行一次
-	once.Do(func() {
-		// 构造消息体
-		requestData := map[string]interface{}{
-			"api_key": data,
-		}
-		// 将数据转换为JSON字符串
-		jsonData, e := json.Marshal(requestData)
-		logger.LogIfR(e)
+	// 构造消息体
+	requestData := map[string]interface{}{
+		"api_key": data,
+	}
+	// 将数据转换为JSON字符串
+	jsonData, e := json.Marshal(requestData)
+	logger.LogIfR(e)
 
-		// 创建一个POST请求
-		request, e := http.NewRequest("POST", url, strings.NewReader(string(jsonData)))
-		logger.LogIfR(e)
+	// 创建一个POST请求
+	request, e := http.NewRequest("POST", url, strings.NewReader(string(jsonData)))
+	logger.LogIfR(e)
 
-		// 设置请求头
+	// 设置请求头
 
-		for key, value := range headers {
-			request.Header.Set(key, value)
-		}
+	for key, value := range headers {
+		request.Header.Set(key, value)
+	}
 
-		// 发送请求
-		response, e := http.DefaultClient.Do(request)
-		logger.LogIfR(e)
+	// 发送请求
+	response, e := http.DefaultClient.Do(request)
+	logger.LogIfR(e)
 
-		defer response.Body.Close()
+	defer response.Body.Close()
 
-		// 读取响应体
-		body, e = io.ReadAll(response.Body)
-		logger.LogIfR(e)
-
-	})
+	// 读取响应体
+	body, e = io.ReadAll(response.Body)
+	logger.LogIfR(e)
 
 	return body, err
 }
